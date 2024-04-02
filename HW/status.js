1834a3fefcea5722d9fe7445ad78ebc55163216c
Morris <morris890717@gmail.com>
2024-04-02 23:15:51 +0800

    remove console log message of function apply

--- HW/status.js
+++ HW/status.js
@@ -40,33 +40,35 @@ addHistory = function (num) {
 };
 
 apply = function (num) {
-  console.log("aaa");
   addHistory(num);
-  console.log("bbb");
   showToast("Apply Successfully", "success", 5000);
-  console.log("ccc");
 };
 
-window.addEventListener('DOMContentLoaded', () => {
-
-	const observer = new IntersectionObserver(entries => {
-		entries.forEach(entry => {
-			const id = entry.target.getAttribute('id');
-			if (entry.intersectionRatio > 0) {
-				document.querySelector(`.sidebar-container ul li a[href="#${id}"]`).parentElement.classList.add('active');
-			} else {
-				document.querySelector(`.sidebar-container ul li a[href="#${id}"]`).parentElement.classList.remove('active');
-			}
-		});
-	}, 
+window.addEventListener("DOMContentLoaded", () => {
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        const id = entry.target.getAttribute("id");
+        if (entry.intersectionRatio > 0) {
+          document
+            .querySelector(`.sidebar-container ul li a[href="#${id}"]`)
+            .parentElement.classList.add("active");
+        } else {
+          document
+            .querySelector(`.sidebar-container ul li a[href="#${id}"]`)
+            .parentElement.classList.remove("active");
+        }
+      });
+    },
     {
-        root: null,
-        rootMargin: "-300px 0px -300px 0px",
-        threshold: 0,
-    });
+      root: null,
+      rootMargin: "-300px 0px -300px 0px",
+      threshold: 0,
+    }
+  );
 
-	// Track all sections that have an `id` applied
-	document.querySelectorAll('section').forEach((section) => {
-		observer.observe(section);
-	});
+  // Track all sections that have an `id` applied
+  document.querySelectorAll("section").forEach((section) => {
+    observer.observe(section);
+  });
 });
