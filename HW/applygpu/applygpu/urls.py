747eb61897707cad7b2228f58610dc6a6f1b04b8
Morris <morris890717@gmail.com>
2024-05-08 01:52:24 +0800

    create homepage and news page, and put news on them

--- HW/applygpu/applygpu/urls.py
+++ HW/applygpu/applygpu/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from mainsite.views import homepage, showNews
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('', homepage),
+    path('news/<slug:slug>/', showNews),
 ]
--- HW/applygpu/mainsite/views.py
+++ HW/applygpu/mainsite/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import News
 
 # Create your views here.
+def homepage(request):
+    newss = News.objects.all()
+    return render(request, 'index.html', locals())
+
+def showNews(request, slug):
+    try:
+        news = News.objects.get(slug=slug)
+        return render(request, 'news.html', locals())
+    except:
+        return redirect('/')
