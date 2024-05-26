2f55d09460280e79e78104d25b955eee277ba363
daniel890104 <daniel01040104@gmail.com>
2024-05-26 19:13:56 +0800

    add Base.htm

--- project/project/urls.py
+++ project/project/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from manage_system.views import homepage
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', homepage),
 ]
