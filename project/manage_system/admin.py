4ba9a15d253168ecd63846e5fae6bcd882b59e52
Morris <morris890717@gmail.com>
2024-06-01 00:21:59 +0800

    add two models 'User' and 'Event'

--- project/manage_system/admin.py
+++ project/manage_system/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
-
+from .models import User, Event
 # Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'createDate')
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('eventName', 'createUserId', 'createUsername', 'numberOfJoinedUsers')
+
+    def createUserId(self, obj):
+        return obj.createUser.id
+    createUserId.short_description = 'Creator ID' # setting title
+
+    def createUsername(self, obj):
+        return obj.createUser.username
+    createUsername.short_description = 'Creator Username' # setting title
+
+    def numberOfJoinedUsers(self, obj):
+        return obj.joinedUsers.count()
+    numberOfJoinedUsers.short_description = '# of Participants' # setting title
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Event, EventAdmin)
--- project/manage_system/models.py
+++ project/manage_system/models.py
@@ -1,3 +1,26 @@
 from django.db import models
-
+from django.conf import settings
+from django.utils import timezone
 # Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=50)
+    createDate = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
+
+class Event(models.Model):
+    eventName = models.CharField(max_length=150)
+    createUser = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL, related_name = 'createdEvents')
+    joinedUsers = models.ManyToManyField(User, related_name='joinedEvents', blank=True)
+    max_limit = models.PositiveIntegerField()
+    createDate = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering=('-createDate',)
+
+    def __str__(self):
+        return self.eventName
