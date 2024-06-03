439d37c76804471379c375fc368de21be9ce220b
Morris <morris890717@gmail.com>
2024-06-03 15:02:58 +0800

    modify eventPage.html

--- project/static/js/themeToggle.js
+++ project/static/js/themeToggle.js
@@ -26,21 +26,21 @@ function toggleTheme() {
   setTheme();
 }
   
-  // Set the initial theme based on localStorage
-  window.onload = function() {
-    var theme = localStorage.getItem('theme');
-    if (theme === 'dark') {
-      document.querySelector('.card').classList.add('is-flipped');
-    }
-    setTheme();
+// Set the initial theme based on localStorage
+window.onload = function() {
+  var theme = localStorage.getItem('theme');
+  if (theme === 'dark') {
+    document.querySelector('.card').classList.add('is-flipped');
+  }
+  setTheme();
   
-    // Add event listener to the toggle button
-    document.getElementById('themeToggle').addEventListener('click', toggleTheme);
+  // Add event listener to the toggle button
+  document.getElementById('themeToggle').addEventListener('click', toggleTheme);
   
-    // Flip animation
-    var card = document.querySelector('.card');
-    card.addEventListener('click', function() {
-      card.classList.toggle('is-flipped');
-    });
-  };
+  // Flip animation
+  var card = document.querySelector('.card');
+  card.addEventListener('click', function() {
+    card.classList.toggle('is-flipped');
+  });
+};
   
